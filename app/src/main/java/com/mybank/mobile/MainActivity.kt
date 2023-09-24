e7fb5a28c346acd58eaf5ff36d64e95cc75dc8de
vipinbpkyr <vipinbpkyr@gmail.com>
2023-09-25 04:01:19 +0530

    vipin first commit v2

--- app/src/main/java/com/mybank/mobile/MainActivity.kt
+++ app/src/main/java/com/mybank/mobile/MainActivity.kt
@@ -3,13 +3,18 @@ package com.mybank.mobile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.mybank.mobile.ui.theme.MyBankTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,10 +36,22 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column {
+
     Text(
         text = "Hello $name!",
         modifier = modifier
     )
+    Spacer(modifier = Modifier.height(10.dp))
+
+        for (i in 1..10) {
+        Text(
+            text = "Hello $i!",
+            modifier = modifier
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+    }
+}
 }
 
 @Preview(showBackground = true)
